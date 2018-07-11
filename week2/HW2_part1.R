ed2ea731047b2ae5cbc5137d9bd90e7f5382616e
Pei4 <40812695+Pei4@users.noreply.github.com>
2018-07-11 12:11:41 +0800

    Plot_Done

--- week2/HW2_part1.R
+++ week2/HW2_part1.R
@@ -1,7 +1,14 @@
 library(ggplot2)
 #單變-類別
-ggplot(iris, aes(x=Species))+
-  geom_bar(fill="lightgreen")
-#雙變-類別&連續
-ggplot(iris, aes(x=Sepal.Length, y=Species))+
+ggplot(iris, aes(x=Species, fill=Species))+
+  geom_bar()
+#單變-連續
+ggplot(iris, aes(x=Sepal.Length, fill=Species))+
+  geom_histogram()+
+  facet_wrap(~Species)
+#雙變-連續&連續
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species))+
   geom_point()
+#雙變-離散&連續
+ggplot(iris, aes(x=Species, y=Sepal.Length, fill=Species))+
+  geom_boxplot()
